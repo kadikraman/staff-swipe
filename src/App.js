3f522fc10163877a2fe1c5a388127c90c7643b2f
Kadi Kraman <hellokadi@gmail.com>
2017-03-27 14:37:48 +0100

    Create a basic UI layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Candidate from './components/Candidate';
+import Checked from './components/Checked';
+import Error from './components/Error';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, world!
+        <h1 className="App__title">ROTA</h1>
+        <Candidate />
+        <div className="App__vote">
+          <span className="App__no"><Error /></span>
+          <span className="App__yes"><Checked /></span>
+        </div>
       </div>
     );
   }
---
+++ src/components/Candidate/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+const Candidate = () => (
+  <div className="Candidate"><img src="http://placehold.it/300x300" alt="test" /></div>
+);
+
+export default Candidate;
